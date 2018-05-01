1e9dbca64f116940e98e73895dd63778955bb339
philipsz-davido <philipsz-davido>
2018-05-01 15:29:08 +0100

    Fixed some merge issues

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * aeth new: starts a new project
+ * aeth start: serves a project
+ * aeth build: builds a project
+ * 
+ * 
+ * new: 
+ * craetes folder
+ * move into folder
+ * create package.json
+ * copy templates into folder
+ * init npm install
+ * 
+ * start:
+ */
