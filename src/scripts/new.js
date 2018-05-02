d67e4b12ce6d2e0bbc3f76d56e26df292ce8ed04
philipsz-davido <philipsz-davido>
2018-05-03 00:11:23 +0100

    modified package.json

--- src/scripts/new.js
+++ src/scripts/new.js
@@ -1,4 +1,6 @@
 const fs = require('fs-extra')
+const { exec, spawn } = require('child_process')
+
 const { templateDir } = require('../../')
 
 function init(projectName) {
@@ -6,9 +8,15 @@ function init(projectName) {
         fs.mkdirSync(projectName)
         fs.copySync(templateDir, process.cwd() + '\\' + projectName)
         console.log(`[${projectName}] created successfully !!`)
+
+        // next, install the project's dependencies
+        npmInstall()
     } else {
         console.log(projectName + ' directory already exist')
     }
 }
 
+function npmInstall() {
+    const child = spawn('npm', ['install'])
+}
 module.exports = init
