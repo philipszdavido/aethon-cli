791331c1c15645954c7a17945af8b50dbbad141d
philipsz-davido <philipsz-davido>
2018-05-10 01:07:55 +0100

    Fixed devServer configurations

--- src/config/webpack.config.devServer.js
+++ src/config/webpack.config.devServer.js
@@ -5,7 +5,7 @@ const appDir = fs.realpathSync(process.cwd())
 const resolVe = rela => path.resolve(appDir, rela)
 
 module.exports = {
-    contentBase: resolVe('dist'),
+    contentBase: resolVe('public'),
     publicPath: '/',
     compress: true,
     //port: 9000
